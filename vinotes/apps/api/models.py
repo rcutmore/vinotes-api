23ca953ce32db50b625cf26a49c2e0a369ecb0f5
Rob Cutmore <robcutmore@gmail.com>
2015-03-22 16:17:33 -0400

    Add initial models for winery, wine, and tasting note.

--- vinotes/apps/api/models.py
+++ vinotes/apps/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Winery(models.Model):
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+class Wine(models.Model):
+    title = models.CharField(max_length=150)
+    vintage = models.IntegerField()
+    winery = models.ForeignKey(Winery)
+
+    def __str__(self):
+        return '{winery} {wine}'.format(winery=self.winery, wine=self.title)
+
+class Note(models.Model):
+    wine = models.ForeignKey(Wine)
+
+    def __str__(self):
+        return 'Tasting note for: {wine}'.format(wine=self.wine)
