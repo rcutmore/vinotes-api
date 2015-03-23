6b2b2d69a9b3a11396c964c1eb01a73db11d4b53
Rob Cutmore <robcutmore@gmail.com>
2015-03-23 07:07:40 -0400

    Add API models to admin page.

--- vinotes/apps/api/admin.py
+++ vinotes/apps/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Note, Wine, Winery
 
-# Register your models here.
+admin.site.register(Note)
+admin.site.register(Wine)
+admin.site.register(Winery)
