ce1afec47987362e72b14abef7577ccc6f53cbc6
Rob Cutmore <robcutmore@gmail.com>
2015-03-25 07:04:11 -0400

    Add description field to Winery and Wine models.

--- vinotes/apps/api/models.py
+++ vinotes/apps/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Winery(models.Model):
     title = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
@@ -9,6 +10,7 @@ class Winery(models.Model):
 class Wine(models.Model):
     title = models.CharField(max_length=150)
     vintage = models.IntegerField()
+    description = models.TextField(blank=True)
     winery = models.ForeignKey(Winery)
 
     def __str__(self):
