d59fc54bdf5eec5fbe24c0a8afca97b60bfe6e6b
Rob Cutmore <robcutmore@gmail.com>
2015-03-27 07:17:42 -0400

    Add Trait model to admin page.

--- vinotes/apps/api/admin.py
+++ vinotes/apps/api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Note, Wine, Winery
+from .models import Note, Trait, Wine, Winery
 
 admin.site.register(Note)
+admin.site.register(Trait)
 admin.site.register(Wine)
 admin.site.register(Winery)
