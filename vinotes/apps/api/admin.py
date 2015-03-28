41b02e3a7a1ed5b4722501e7e92758719987f66e
Rob Cutmore <robcutmore@gmail.com>
2015-03-28 13:14:49 -0400

    Update admin page for Note.

--- vinotes/apps/api/admin.py
+++ vinotes/apps/api/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Note, Trait, Wine, Winery
 
-admin.site.register(Note)
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    filter_horizontal = (
+        'color_traits',
+        'nose_traits',
+        'taste_traits',
+        'finish_traits',
+    )
+
 admin.site.register(Trait)
 admin.site.register(Wine)
 admin.site.register(Winery)
