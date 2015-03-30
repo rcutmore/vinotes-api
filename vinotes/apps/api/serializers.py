c7da0ed13838150f0276c4c9f425390822b5b43b
Rob Cutmore <robcutmore@gmail.com>
2015-03-30 19:18:50 -0400

    Add serializers for API models.

---
+++ vinotes/apps/api/serializers.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note, Trait, Wine, Winery
+
+
+class WinerySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Winery
+        fields = ('id', 'name')
+
+
+class WineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wine
+        fields = ('id', 'winery', 'name', 'vintage')
+
+
+class TraitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trait
+        fields = ('id', 'name')
+
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ('id', 'taster', 'tasted', 'wine', 'color_traits', 
+                  'nose_traits', 'taste_traits', 'finish_traits', 'rating')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'notes')
