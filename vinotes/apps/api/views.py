f7408271e51fe5116a4b9cbcb23fe7d6a605b9e1
Rob Cutmore <robcutmore@gmail.com>
2015-03-31 07:22:44 -0400

    Add initial views for note list and note detail.

--- vinotes/apps/api/views.py
+++ vinotes/apps/api/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import Note, Trait, Wine, Winery
+from .serializers import NoteSerializer, TraitSerializer, WineSerializer, WinerySerializer
 
-# Create your views here.
+
+class JSONResponse(HttpResponse):
+    """
+    An HttpResponse that renders its content into JSON.
+    """
+    def __init__(self, data, **kwargs):
+        content = JSONRenderer().render(data)
+        kwargs['content_type'] = 'application/json'
+        super(JSONResponse, self).__init__(content, **kwargs)
+
+
+@csrf_exempt
+def note_list(request):
+    """
+    List all tasting notes, or create a new tasting note.
+    """
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = NoteSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data, status=201)
+        return JSONReponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def note_detail(request, pk):
+    """
+    Retrieve, update, or delete a tasting note.
+    """
+    try:
+        note = Note.objects.get(pk=pk)
+    except:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = NoteSerializer(note)
+        return JSONResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = NoteSerializer(note, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JSONResponse(serializer.data)
+        return JSONResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        note.delete()
+        return HttpResponse(status=204)
