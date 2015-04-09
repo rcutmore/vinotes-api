9ff64fa4fe884b8c255737c38e9e6d108f16aec9
Rob Cutmore <robcutmore@gmail.com>
2015-04-09 07:24:59 -0400

    Update docstring for IsSameUserOrAdmin permission.

--- vinotes/apps/api/permissions.py
+++ vinotes/apps/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsSameUserOrAdmin(permissions.BasePermission):
     """
-    Custom permissions to only allow same user or admins to view user details.
+    Custom permission to only allow same user or admin to view user details.
     """
     def has_object_permission(self, request, view, user):
         return request.user == user or request.user.is_staff
