e9c2aff0bbe1c2bf2980e20087e6e0af52ff118a
Rob Cutmore <robcutmore@gmail.com>
2015-04-13 07:19:55 -0400

    Reformat code in models.py.

--- vinotes/apps/api/models.py
+++ vinotes/apps/api/models.py
@@ -1,34 +1,44 @@
 from django.db import models
 
+
 class Winery(models.Model):
     name = models.CharField(max_length=150)
+
 
     def __str__(self):
         return self.name
 
+
     class Meta:
         verbose_name_plural = 'wineries'
+
 
 class Wine(models.Model):
     winery = models.ForeignKey(Winery, related_name='wines')
     name = models.CharField(max_length=150)
     vintage = models.IntegerField()
 
+
     def __str__(self):
         return '{winery} {wine} {vintage}'.format(
             winery=self.winery, wine=self.name, vintage=self.vintage)
 
+
     class Meta:
         unique_together = ('winery', 'name', 'vintage',)
+
 
 class Trait(models.Model):
     name = models.CharField(max_length=100, unique=True)
 
+
     def __str__(self):
         return self.name
 
+
 class Note(models.Model):
     RATING_CHOICES = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5),)
+
 
     taster = models.ForeignKey('auth.User', related_name='notes')
     tasted = models.DateTimeField(null=True, blank=True)
@@ -43,6 +53,7 @@ class Note(models.Model):
         Trait, related_name='finish_notes', null=True, blank=True)
     rating = models.PositiveSmallIntegerField(
         choices=RATING_CHOICES, null=True, blank=True)
+
 
     def __str__(self):
         return 'Tasting note for: {wine}'.format(wine=self.wine)
