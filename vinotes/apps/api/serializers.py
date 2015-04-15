137e12131f84aeb63e52d91a384ce3422dd8760e
Rob Cutmore <robcutmore@gmail.com>
2015-04-15 07:16:00 -0400

    Update user serializer to hash password when registering.

--- vinotes/apps/api/serializers.py
+++ vinotes/apps/api/serializers.py
@@ -55,3 +55,14 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ('url', 'username', 'email', 'password', 'notes')
         write_only_fields = ('password',)
+
+
+    def create(self, validated_data):
+        """
+        Make sure user's password is hashed before storing.
+        """
+        user = User(
+            email=validated_data['email'], username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
