a12e7edbfd8ca3fdfe1cd21302dc627c2ede1128
Rob Cutmore <robcutmore@gmail.com>
2015-04-18 17:47:37 -0400

    Add tests for creating a winery.

--- vinotes/apps/api/tests.py
+++ vinotes/apps/api/tests.py
@@ -36,12 +36,50 @@ class TraitTests(APITestCase):
         new_trait_url = reverse('trait-detail', kwargs={'pk': 1})
         self.assertTrue(new_trait_url in response.data['url'])
 
+
     def test_create_trait_without_authentication(self):
         """
-        Ensure that we cannot create a new trait without loggin in.
+        Ensure that we cannot create a new trait without logging in.
         """
         # Send POST request to create trait.
         url = reverse('trait-list')
+        data = {'name': 'test'}
+        response = self.client.post(url, data, format='json')
+
+        # Make sure authentication error was returned.
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue('name' not in response.data)
+
+
+class WineryTests(APITestCase):
+    def setUp(self):
+        add_user('test', 'test@test.com')
+
+
+    def test_create_winery_with_authentication(self):
+        """
+        Ensure that we can create a new winery after logging in.
+        """
+        self.client.login(username='test', password='test')
+
+        # Send POST request to create winey.
+        url = reverse('winery-list')
+        data = {'name': 'test'}
+        response = self.client.post(url, data, format='json')
+
+        # Make sure winey was created with expected data.
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], data['name'])
+        new_wine_url = reverse('winery-detail', kwargs={'pk': 1})
+        self.assertTrue(new_wine_url in response.data['url'])
+
+
+    def test_create_winery_without_authentication(self):
+        """
+        Ensure that we cannot create a new winery without logging in.
+        """
+        # Send POST request to create winery.
+        url = reverse('winery-list')
         data = {'name': 'test'}
         response = self.client.post(url, data, format='json')
 
