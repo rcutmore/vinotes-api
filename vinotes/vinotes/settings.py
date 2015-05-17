56094f73cbae2878cf2c0b6588428ea6bd85a7c1
Rob Cutmore <robcutmore@gmail.com>
2015-05-17 14:29:30 -0400

    Enable both token and session authentication.

--- vinotes/vinotes/settings.py
+++ vinotes/vinotes/settings.py
@@ -105,6 +105,10 @@ STATIC_URL = '/static/'
 # Django Rest Framework
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'PAGE_SIZE': 10,
 }
 
--- vinotes/vinotes/urls.py
+++ vinotes/vinotes/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^', include('apps.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
 )
