6c85385f921e14a628fe8e44421f84c939c4b373
Rob Cutmore <robcutmore@gmail.com>
2015-11-28 09:20:31 -0500

    Reformat URL files

--- vinotes/apps/api/urls.py
+++ vinotes/apps/api/urls.py
@@ -1,3 +1,6 @@
+"""
+Contains API app URLs.
+"""
 from django.conf.urls import url
 
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -6,37 +9,69 @@ from . import views
 
 
 urlpatterns = format_suffix_patterns([
-    url(r'^$', 
-        views.api_root, 
+    # API root.
+    # Ex: /
+    url(r'^$',
+        views.api_root,
         name='api-root'),
-    url(r'^notes/$', 
-        views.NoteList.as_view(), 
+
+    # Fetches list of notes.
+    # Ex: /notes/
+    url(r'^notes/$',
+        views.NoteList.as_view(),
         name='note-list'),
-    url(r'^notes/(?P<pk>[0-9]+)/$', 
-        views.NoteDetail.as_view(), 
+
+    # Fetches details for single note.
+    # Ex: /notes/1/
+    url(r'^notes/(?P<pk>[0-9]+)/$',
+        views.NoteDetail.as_view(),
         name='note-detail'),
-    url(r'^traits/$', 
-        views.TraitList.as_view(), 
+
+    # Fetches list of traits.
+    # Ex: /traits/
+    url(r'^traits/$',
+        views.TraitList.as_view(),
         name='trait-list'),
-    url(r'^traits/(?P<pk>[0-9]+)/$', 
-        views.TraitDetail.as_view(), 
+
+    # Fetches details for single trait.
+    # Ex: /traits/1/
+    url(r'^traits/(?P<pk>[0-9]+)/$',
+        views.TraitDetail.as_view(),
         name='trait-detail'),
-    url(r'^wines/$', 
-        views.WineList.as_view(), 
+
+    # Fetches list of wines.
+    # Ex: /wines/
+    url(r'^wines/$',
+        views.WineList.as_view(),
         name='wine-list'),
-    url(r'^wines/(?P<pk>[0-9]+)/$', 
-        views.WineDetail.as_view(), 
+
+    # Fetches details for single wine.
+    # Ex: /wines/1/
+    url(r'^wines/(?P<pk>[0-9]+)/$',
+        views.WineDetail.as_view(),
         name='wine-detail'),
-    url(r'^wineries/$', 
-        views.WineryList.as_view(), 
+
+    # Fetches list of wineries.
+    # Ex: /wineries/
+    url(r'^wineries/$',
+        views.WineryList.as_view(),
         name='winery-list'),
-    url(r'^wineries/(?P<pk>[0-9]+)/$', 
-        views.WineryDetail.as_view(), 
+
+    # Fetches details for single winery.
+    # Ex: /wineries/1/
+    url(r'^wineries/(?P<pk>[0-9]+)/$',
+        views.WineryDetail.as_view(),
         name='winery-detail'),
-    url(r'^users/$', 
-        views.UserList.as_view(), 
+
+    # Fetches list of users.
+    # Ex: /users/
+    url(r'^users/$',
+        views.UserList.as_view(),
         name='emailuser-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', 
-        views.UserDetail.as_view(), 
+
+    # Fetches details for single user.
+    # Ex: /users/1/
+    url(r'^users/(?P<pk>[0-9]+)/$',
+        views.UserDetail.as_view(),
         name='emailuser-detail'),
 ])
--- vinotes/vinotes/urls.py
+++ vinotes/vinotes/urls.py
@@ -1,10 +1,24 @@
-from django.conf.urls import patterns, include, url
+"""
+Contains main project URLs.
+"""
+from django.conf.urls import include, url
 from django.contrib import admin
 
 
-urlpatterns = patterns('',
-    url(r'^', include('apps.api.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),
-)
+urlpatterns = [
+    # All API URLs.
+    url(r'^',
+        include('apps.api.urls')),
+
+    # All admin URLs.
+    url(r'^admin/',
+        include(admin.site.urls)),
+
+    # URLs for django-rest-framework authentiction.
+    url(r'^api-auth/',
+        include('rest_framework.urls', namespace='rest_framework')),
+
+    # URLs for django-rest-framework token authentication.
+    url(r'^api-token-auth/',
+        'rest_framework.authtoken.views.obtain_auth_token'),
+]
