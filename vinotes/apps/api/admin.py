42c1f11705e7818a9a0a35c64abc8610652ce406
Rob Cutmore <robcutmore@gmail.com>
2015-11-28 09:41:23 -0500

    Reformat admin.py for API app

--- vinotes/apps/api/admin.py
+++ vinotes/apps/api/admin.py
@@ -1,7 +1,12 @@
+"""
+Contains API app model registrations for admin site.
+"""
 from django.contrib import admin
+
 from .models import Note, Trait, Wine, Winery
 
 
+# Customize note's trait selection on admin site.
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
     filter_horizontal = (
@@ -10,7 +15,6 @@ class NoteAdmin(admin.ModelAdmin):
         'taste_traits',
         'finish_traits',
     )
-
 
 admin.site.register(Trait)
 admin.site.register(Wine)
