7a2d96b9d602694b05a251889de725c5aa5aad6b
Rob Cutmore <robcutmore@gmail.com>
2015-11-28 09:49:12 -0500

    Reformat permissions.py for API app

--- vinotes/apps/api/permissions.py
+++ vinotes/apps/api/permissions.py
@@ -1,9 +1,11 @@
+"""
+Contains custom permissions for API app.
+"""
 from rest_framework import permissions
 
 
 class IsAuthenticatedOrRegistering(permissions.BasePermission):
-    """
-    Custom permission to allow access to authenticated users or those registering.
-    """
+    """Allow access for authenticated users or those registering."""
+
     def has_permission(self, request, view):
         return request.user.is_authenticated() or request.method == 'POST'
